89cd8350a7fe92ba325986c6eda799ae420cf30c
DnlGalvan <84108325+DnlGalvan@users.noreply.github.com>
2021-05-12 20:57:45 -0300

    Primeira versão do projeto

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import {useState} from 'react'
+
+function App() {
+
+  const [lista, setLista] = useState([])
+  
+  function adicionar(){
+    var texto = document.getElementById("texto").value
+    setLista([...lista, texto])
+    document.getElementById('texto').value = ''
+  }
+
+  function remover(posicao) {
+    var novalista = lista.filter((item, index) => index !== posicao)
+    setLista(novalista)
+  }
+
+  return (
+    <main>
+      <input type="text" id="texto"/>
+      <button onClick={adicionar}>Adicionar</button>
+      {
+        lista.map( (item, index) => (
+          <div>
+            <br/>
+            <p>{item}</p>
+            <button onClick={() => remover(index)}>X</button>
+          </div>
+        ))
+      }
+    </main>
+  );
+}
+
+export default App;
